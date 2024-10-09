c7b889b3b02a69f67d7f65204eb77087b912118e
awabofficial <awab.teto.official@gmail.com>
2024-10-09 17:54:05 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: 'My Sticker Pack',
-  author: 'My Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'تيتو لديكم لا خوف عليكم',
+  author: 'انستا awabofficial 🛩️🔥',
+  botName: "بوت تيتو ❤️",
+  botOwner: 'تيتو شخصيا بحبك', // Your name
+  ownerNumber: '249124702614', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "تيتو لديكم لا خوف عليكم",
   version: "1.0.0",
 };
 
